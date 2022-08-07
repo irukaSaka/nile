a5d1b92a5e088a42c7aad3726c0d36e7c6211c28
ruka <saka.iruka.noel@gmail.com>
2022-08-08 08:44:14 +0900

    .

--- src/components/admin/pages/UserManagement.tsx
+++ src/components/admin/pages/UserManagement.tsx
@@ -1,3 +1,25 @@
-import { memo, FC } from "react"
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react"
+import { memo, FC, useEffect } from "react"
+import { UseAllUsers } from "../../hooks/api/admin/useAllUsers"
 
-export const UserManagement:FC = memo(() => (<p>ユーザー管理画面です</p>))
+export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = UseAllUsers();
+  useEffect(() => getUsers(), [])
+  return (
+    <>
+      {
+        loading ?
+          (
+            <Center>
+              <Spinner />
+            </Center>
+          ) :
+          (
+            <Wrap>
+              <WrapItem />
+            </Wrap>
+          )
+      }
+    </>
+  )
+})
---
+++ src/components/hooks/api/admin/useAllUsers.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { User } from "../../../types/api/user";
+import { useMessage } from "../../useMessage";
+
+export const UseAllUsers = () => {
+  const { showMessage } = useMessage();
+
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState<Array<User>>([]);
+
+  const getUsers = useCallback(() => {
+    setLoading(true);
+    axios.get<Array<User>>("http://etes")
+      .then((res) => setUsers(res.data))
+      .catch(() => {
+        showMessage({ title: "ユーザー取得に失敗しました", status: "error" });
+      })
+      .finally(() => setLoading(false))
+  }, [showMessage])
+  return { getUsers, users, loading }
+}
